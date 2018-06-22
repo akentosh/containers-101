c6d5ba093398a52a661a4e2cdba1788f5741ec44
akentosh <akentosh@yahoo.com>
2018-06-22 10:40:18 -0400

    Adding container images for app

--- app/app.py
+++ app/app.py
@@ -3,20 +3,13 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# list of container images
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/14/enhanced-buzz-8423-1381861722-1.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/15/enhanced-buzz-24732-1381864258-19.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/13/enhanced-buzz-24812-1381858809-8.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/15/enhanced-buzz-24745-1381864635-20.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/15/enhanced-buzz-orig-17356-1381866444-13.jpg?downsize=715:*&output-format=auto&output-quality=auto"
 ]
 
 @app.route('/')
